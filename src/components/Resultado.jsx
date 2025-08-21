be3853ee7f6dc70363127d4bd96b5615d985fe27
Lucas Toledo Cortonezi <lucastoledocort25@gmail.com>
2025-08-21 11:36:35 -0300

    Aula 21/08 react projeto inicial de IMC

---
+++ src/components/Resultado.jsx
@@ -0,0 +1,68 @@
+import "../css/global.css"
+import "../css/resultado.css"
+
+const TabelaImc=(resultado)=>{
+    if(resultado < 18) {
+        return(
+            <>
+            <td>Abaixo do Peso</td>
+            <td>Abaixo de 18</td>
+            </>
+        );
+    }else if(resultado > 18 && resultado < 25) {
+        return(
+            <>
+            <td>Peso Normal</td>
+            <td>18 - 24.9</td>
+            </>
+        );
+    }else if(resultado > 24 &&  resultado < 30) {
+        return(
+            <>
+            <td>Sobre Peso</td>
+            <td>25 - 29.9</td>
+            </>
+        );
+    }else if(resultado > 30 && resultado < 35){
+        return(
+            <>
+            <td>Obesidade Grau-I</td>
+            <td>30 - 34.9</td>
+            </>
+        );
+    }else{
+        return(
+            <>
+            <td>Obesidade Mórbida</td>
+            <td>Maior ou a 40</td>
+            </>
+        )
+    }
+
+    }
+
+
+
+function Resultado({resultado}){
+    return(
+        <div>
+            <div className="tabela">
+                <h2>
+                    Seu IMC é de <span className="imcSpan">{resultado}</span>
+                </h2>
+            </div>
+            <table className="tabela">
+                <thead className="tabelaHeader">
+                    <tr>
+                        <th>Classificação</th>
+                        <th>IMC</th>
+                    </tr>
+                </thead>
+                    <tbody className="tabelaBody">
+                        <tr>{TabelaImc(resultado)}</tr>
+                    </tbody>
+            </table>
+        </div>
+    )
+}
+export default Resultado
