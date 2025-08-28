9a9cc23f54fe9c3213d23cfd4d9888e0fd8447f5
Lucas Toledo Cortonezi <lucastoledocort25@gmail.com>
2025-08-28 11:37:35 -0300

    calculadora

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,64 @@
-import Header from "./components/Header"
-function App() {
+import { useState } from "react";
+import Header from "./components/Header";
+import Resultado from "./components/Resultado";
+import "./css/estilo.css";
+import "./css/global.css";
 
+function App() {
+ // hook usestate manipula o estado da variável
+
+ const[altura,setAltura]=useState(0);
+  const [peso,setPeso] =useState(0);
+  const [resultado,setResultado]=useState(0);
+
+  const [mostrarResultado,setMostrarResultado]=useState(false);
+  //criando função calcular imc
+
+  const calcularImc=(e)=>{
+    e.preventDefault(); //evita o recarregamento da página
+    if(altura > 0 && peso > 0){
+      const imc=peso / (altura*altura);
+      setResultado(imc.toFixed(2)); //arredonda para 2 casas decimais
+      setMostrarResultado(true);
+    } else{
+      alert("Por favor digite valores válidos");
+      setMostrarResultado(false);
+    }
+  }
 
   return (
-    <>
-      <Header/>
+    <div className="container">
+      <div className="box">
+        <Header/>
+        <form>
+          <div>
+            <label htmlfor="altura"><span>(exemplo:1.80)</span></label>
+            <input 
+            type="number"
+            id = "altura"
+            placeholder="Digite sua altura" 
+            value={altura}
+            onChange={(e)=>setAltura(parseFloat(e.target.value))}
+            />
+          </div>
 
-    </>
+          <div>
+            <label htmlfor="peso"><span>(exemplo:80)</span></label>
+            <input 
+            type="number"
+            id = "peso"
+            placeholder="Digite seu peso" 
+            value={peso}
+            onChange={(e)=>setPeso(parseFloat(e.target.value))}/>
+          </div>
+          <button onClick={calcularImc}>Calcular</button>
+          
+        </form>
+      </div>
+      {mostrarResultado &&(
+        <Resultado resultado={resultado}/>
+      )}
+    </div>
   )
 }
 
--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,48 +1,6 @@
 import "../css/global.css"
 import "../css/resultado.css"
-
-const TabelaImc=(resultado)=>{
-    if(resultado < 18) {
-        return(
-            <>
-            <td>Abaixo do Peso</td>
-            <td>Abaixo de 18</td>
-            </>
-        );
-    }else if(resultado > 18 && resultado < 25) {
-        return(
-            <>
-            <td>Peso Normal</td>
-            <td>18 - 24.9</td>
-            </>
-        );
-    }else if(resultado > 24 &&  resultado < 30) {
-        return(
-            <>
-            <td>Sobre Peso</td>
-            <td>25 - 29.9</td>
-            </>
-        );
-    }else if(resultado > 30 && resultado < 35){
-        return(
-            <>
-            <td>Obesidade Grau-I</td>
-            <td>30 - 34.9</td>
-            </>
-        );
-    }else{
-        return(
-            <>
-            <td>Obesidade Mórbida</td>
-            <td>Maior ou a 40</td>
-            </>
-        )
-    }
-
-    }
-
-
-
+import TabelaImc from "./TabelaImc"
 function Resultado({resultado}){
     return(
         <div>
